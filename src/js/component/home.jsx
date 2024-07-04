bda74c1e549df675b87dd5e3f70b17bee5538390
Agustin2799 <140901456+Agustin2799@users.noreply.github.com>
2024-07-04 21:36:53 +0000

    Finalizando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,25 @@
 import React from "react";
+import Nav from "./nav"
+import Card from "./card"
+import Jumbotron from "./jumbotron";
+import Footer from "./footer";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<Nav />
+			<div className="container">
+				<Jumbotron />
+				<div className="row d-flex justify-content-center">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				</div>
+			</div>
+			<Footer/>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Footer from "./component/footer.jsx";
 
 //render your react application
 ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
